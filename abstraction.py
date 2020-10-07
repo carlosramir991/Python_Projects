f2ce95faccd87b20565a57939735e1046e320c04
carlosramir991 <65266113+carlosramir991@users.noreply.github.com>
2020-10-07 13:48:41 -0600

    Add files via upload

---
+++ abstraction.py
@@ -0,0 +1,23 @@
+
+from abc import ABC, abstractmethod
+
+class calories(ABC):
+
+    def maint(self,calories):
+        print("Your maintenance calories today will be {}!".format(calories))
+
+    @abstractmethod
+    def lose(self,calories):
+        pass
+
+class losingw(calories):
+
+    
+    def lose(self,calories):
+        calories = (calories -250)
+        print("In order to lose weight you need to eat {} calories!".format(calories))
+
+        
+obj = calories()
+obj.maint(2000)
+obj.lose(2000)
